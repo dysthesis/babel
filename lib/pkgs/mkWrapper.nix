17cd5104120ec4252d5508f544a2701d18281f99
Dysthesis <antheoraviel@protonmail.com>
2025-02-14 10:20:40 +1100

    Add documentation comments

--- lib/pkgs/mkWrapper.nix
+++ lib/pkgs/mkWrapper.nix
@@ -1,16 +1,32 @@
+# Wraps a binary with the given postBuild argument
+#
+# Taken from:
+# https://github.com/hlissner/dotfiles/blob/00d358f060f79c9f406d20dd659e109b44948370/lib/pkgs.nix#L17-L24
+#
+# Usage:
+# mkWrapper program ''
+#   wrapProgram "$out/bin/program" --add-flags "${flags}" \
+#                                  --run "${run}"
+# ''
 let
-  inherit (builtins)
+  inherit
+    (builtins)
     isList
     elemAt
     ;
 in
-pkgs: package: postBuild:
-let
-  name = if isList package then elemAt package 0 else package;
-  paths = if isList package then package else [ package ];
-in
-pkgs.symlinkJoin {
-  inherit paths postBuild;
-  name = "${name}-wrapped";
-  buildInputs = [ pkgs.makeWrapper ];
-}
+  pkgs: package: postBuild: let
+    name =
+      if isList package
+      then elemAt package 0
+      else package;
+    paths =
+      if isList package
+      then package
+      else [package];
+  in
+    pkgs.symlinkJoin {
+      inherit paths postBuild;
+      name = "${name}-wrapped";
+      buildInputs = [pkgs.makeWrapper];
+    }
