f7a172fc349b525e7d787e780ebbce02accd7d38
Dysthesis <antheoraviel@protonmail.com>
2025-02-13 17:22:32 +1100

    fixes

--- lib/path/getDirectories.nix
+++ lib/path/getDirectories.nix
@@ -1,10 +1,15 @@
 lib:
 let
   inherit (builtins)
+	  attrNames
     readDir
     ;
   inherit (lib)
     filterAttrs
     ;
 in
-dir: dir |> readDir |> filterAttrs (_name: value: value == "directory") |> attrNames
+dir: 
+  dir 
+	|> readDir 
+	|> filterAttrs (_name: value: value == "directory") 
+	|> attrNames
