85c8e5c45a9953005bad50ff40be1c01ff5f3edb
Dysthesis <antheoraviel@protonmail.com>
2025-02-21 15:15:49 +1100

    Did I fuck up the ordering?

--- lib/system/mkSystem.nix
+++ lib/system/mkSystem.nix
@@ -1,41 +1,42 @@
-lib:
-let
-  inherit (lib)
+lib: let
+  inherit
+    (lib)
     nixosSystem
     mkDefault
     checkListOfEnum
     ;
   inherit (lib.babel.path) getDirectories;
 in
-{
-  self,
-  system,
-  hostname,
-  config,
-  inputs,
-  specialArgs,
-  profiles ? [ ],
-  ...
-}:
-nixosSystem {
-  inherit system;
-  modules = [
-    { networking.hostName = hostname; }
-    { nixpkgs.hostPlatform = mkDefault system; }
+  {
+    self,
+    system,
+    hostname,
+    config,
+    inputs,
+    specialArgs,
+    profiles ? [],
+    ...
+  }:
+    nixosSystem {
+      inherit system;
+      modules = [
+        {networking.hostName = hostname;}
+        {nixpkgs.hostPlatform = mkDefault system;}
 
-    (
-      { modulesPath, ... }:
-      let
-        profilesPath = "${modulesPath}/profiles";
-        validModules = getDirectories profilesPath;
-      in
-      checkListOfEnum "valid modules" profiles validModules {
-        imports = map profiles (profile: "${profilesPath}/${profile}.nix");
-      }
-    )
-    config
-  ];
-  specialArgs = {
-    inherit self inputs;
-  } // specialArgs;
-}
+        (
+          {modulesPath, ...}: let
+            profilesPath = "${modulesPath}/profiles";
+            validProfiles = getDirectories profilesPath;
+          in
+            checkListOfEnum "valid modules" validProfiles profiles {
+              imports = map profiles (profile: "${profilesPath}/${profile}.nix");
+            }
+        )
+        config
+      ];
+      specialArgs =
+        {
+          inherit self inputs;
+        }
+        // specialArgs;
+    }
