df7421f8e961bfd1be8afc63b6867b40a8084b0d
Dysthesis <antheoraviel@protonmail.com>
2025-02-27 12:17:34 +1100

    Whoopsie

--- lib/forAllSystems.nix
+++ lib/forAllSystems.nix
@@ -3,4 +3,4 @@ nixpkgs: {
   overlays ? [],
   systems ? [],
 }: f:
-nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system overlays;}))
+pkgs.lib.genAttrs systems (system: f (import pkgs {inherit system overlays;}))
