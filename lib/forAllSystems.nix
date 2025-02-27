576e8cb5d2323d70c690366f391411d32b59b194
Dysthesis <antheoraviel@protonmail.com>
2025-02-27 12:20:14 +1100

    nvm

--- lib/forAllSystems.nix
+++ lib/forAllSystems.nix
@@ -1,6 +1,5 @@
 nixpkgs: {
-  pkgs ? nixpkgs,
   overlays ? [],
   systems ? [],
 }: f:
-pkgs.lib.genAttrs systems (system: f (import pkgs {inherit system overlays;}))
+nixpkgs.lib.genAttrs systems (system: f (import nixpkgs {inherit system overlays;}))
