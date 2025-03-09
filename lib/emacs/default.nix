2cfc3de6be9727286e22ac2684a6d7fae900ba46
Dysthesis <antheoraviel@protonmail.com>
2025-03-09 18:25:51 +1100

    Changes

--- lib/emacs/default.nix
+++ lib/emacs/default.nix
@@ -1,4 +1,4 @@
-pkgs: lib: {
+lib: {
   orgTangle = import ./orgTangle.nix;
   orgTangleToString = import ./orgTangleToString.nix lib;
 }
--- lib/system/mkSystem.nix
+++ lib/system/mkSystem.nix
@@ -1,6 +1,6 @@
-lib: let
-  inherit
-    (lib)
+lib:
+let
+  inherit (lib)
     nixosSystem
     mkDefault
     checkListOfEnum
@@ -8,35 +8,35 @@ lib: let
     ;
   inherit (lib.babel.path) getFiles;
 in
-  {
-    self,
-    system,
-    hostname,
-    config,
-    inputs,
-    specialArgs,
-    profiles ? [],
-    ...
-  }:
-    nixosSystem {
-      inherit system;
-      modules = [
-        {networking.hostName = hostname;}
-        {nixpkgs.hostPlatform = mkDefault system;}
+{
+  self,
+  system,
+  hostname,
+  config,
+  inputs,
+  specialArgs,
+  profiles ? [ ],
+  ...
+}:
+nixosSystem {
+  inherit system;
+  modules = [
+    { networking.hostName = hostname; }
+    { nixpkgs.hostPlatform = mkDefault system; }
 
-        ({modulesPath, ...}: let
-          profilesPath = "${modulesPath}/profiles";
-          validProfiles = map (removeSuffix ".nix") (getFiles profilesPath);
-        in
-          (checkListOfEnum "valid modules" validProfiles profiles)
-          {
-            imports = map (profile: "${profilesPath}/${profile}.nix") profiles;
-          })
-        config
-      ];
-      specialArgs =
-        {
-          inherit self inputs;
-        }
-        // specialArgs;
-    }
+    (
+      { modulesPath, ... }:
+      let
+        profilesPath = "${modulesPath}/profiles";
+        validProfiles = map (removeSuffix ".nix") (getFiles profilesPath);
+      in
+      (checkListOfEnum "valid modules" validProfiles profiles) {
+        imports = map (profile: "${profilesPath}/${profile}.nix") profiles;
+      }
+    )
+    config
+  ];
+  specialArgs = {
+    inherit self inputs;
+  } // specialArgs;
+}
