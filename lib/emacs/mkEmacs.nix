e61b624e2e2e3ca87497dd0953d0ad011a8d510a
Dysthesis <antheoraviel@protonmail.com>
2025-03-10 16:00:42 +1100

    Initial emacs shennanigans

---
+++ lib/emacs/mkEmacs.nix
@@ -0,0 +1,101 @@
+/*
+From: https://github.com/nix-community/emacs-overlay/blob/7236e0baef0a94fa7826f034ea96433224bcea89/elisp.nix
+Parse an emacs lisp configuration file to derive packages from
+use-package declarations.
+*/
+{pkgs}: let
+  inherit (lib.babel.emacs) parsePackagesFromUsePackage;
+  inherit (pkgs) lib;
+in
+  {
+    config,
+    # bool to use the value of config or a derivation whose name is default.el
+    defaultInitFile ? false,
+    # emulate `use-package-always-ensure` behavior (defaulting to false)
+    alwaysEnsure ? false,
+    # emulate `#+PROPERTY: header-args:emacs-lisp :tangle yes`
+    alwaysTangle ? false,
+    extraEmacsPackages ? epkgs: [],
+    package ? pkgs.emacs,
+    override ? (self: super: {}),
+  }: let
+    isOrgModeFile = let
+      ext = lib.last (builtins.split "\\." (builtins.toString config));
+      type = builtins.typeOf config;
+    in
+      (type == "path" || lib.hasPrefix "/" config) && ext == "org";
+
+    configText = let
+      type = builtins.typeOf config;
+    in
+      # configText can be sourced from either:
+      # - A string with context { config = "${hello}/config.el"; }
+      if type == "string" && builtins.hasContext config && lib.hasPrefix builtins.storeDir config
+      then builtins.readFile config
+      # - A config literal { config = "(use-package foo)"; }
+      else if type == "string"
+      then config
+      # - A config path { config = ./config.el; }
+      else if type == "path"
+      then builtins.readFile config
+      # - A derivation { config = pkgs.writeText "config.el" "(use-package foo)"; }
+      else if lib.isDerivation config
+      then builtins.readFile "${config}"
+      else throw "Unsupported type for config: \"${type}\"";
+
+    packages = parsePackagesFromUsePackage {
+      inherit configText isOrgModeFile alwaysTangle alwaysEnsure;
+    };
+    emacsPackages = (pkgs.emacsPackagesFor package).overrideScope (
+      self: super:
+      # for backward compatibility: override was a function with one parameter
+        if builtins.isFunction (override super)
+        then override self super
+        else override super
+    );
+    emacsWithPackages = emacsPackages.emacsWithPackages;
+    mkPackageError = name: let
+      errorFun =
+        if (alwaysEnsure != null && alwaysEnsure)
+        then builtins.trace
+        else throw;
+    in
+      errorFun "Emacs package ${name}, declared wanted with use-package, not found." null;
+  in
+    emacsWithPackages (epkgs: let
+      usePkgs = map (name: epkgs.${name} or (mkPackageError name)) packages;
+      extraPkgs = extraEmacsPackages epkgs;
+      defaultInitFilePkg =
+        if !((builtins.isBool defaultInitFile) || (lib.isDerivation defaultInitFile))
+        then throw "defaultInitFile must be bool or derivation"
+        else if defaultInitFile == false
+        then null
+        else let
+          # name of the default init file must be default.el according to elisp manual
+          defaultInitFileName = "default.el";
+          configFile = pkgs.writeText defaultInitFileName configText;
+          orgModeConfigFile =
+            pkgs.runCommand defaultInitFileName {
+              nativeBuildInputs = [package];
+            } ''
+              cp ${configFile} config.org
+              emacs -Q --batch ./config.org -f org-babel-tangle
+              mv config.el $out
+            '';
+        in
+          epkgs.trivialBuild {
+            pname = "default";
+            src =
+              if defaultInitFile == true
+              then
+                if isOrgModeFile
+                then orgModeConfigFile
+                else configFile
+              else if defaultInitFile.name == defaultInitFileName
+              then defaultInitFile
+              else throw "name of defaultInitFile must be ${defaultInitFileName}";
+            version = "0.1.0";
+            packageRequires = usePkgs ++ extraPkgs;
+          };
+    in
+      usePkgs ++ extraPkgs ++ [defaultInitFilePkg])
---
+++ lib/emacs/parsePackagesFromUsePackage.nix
@@ -0,0 +1,105 @@
+# From https://github.com/nix-community/emacs-overlay/blob/7236e0baef0a94fa7826f034ea96433224bcea89/parse.nix
+# Get a list of packages declared wanted with `use-package` in the
+# input string `config`. The goal is to only list packages that
+# would be installed by `use-package` on evaluation; thus we look at
+# the `:ensure` and `:disabled` keyword values to attempt to figure
+# out which and whether the package should be installed.
+#
+# Example input:
+#
+# ''
+#   (use-package org
+#     :commands org-mode
+#     :bind (("C-c a" . org-agenda)
+#            :map org-mode-map
+#            ([C-right] . org-demote-subtree)
+#            ([C-left] . org-promote-subtree)))
+#
+#   (use-package direnv
+#     :ensure t
+#     :config (direnv-mode))
+#
+#   (use-package paredit-mode
+#     :ensure paredit
+#     :hook (emacs-lisp-mode lisp-mode lisp-interaction-mode))
+# ''
+# => [ "direnv" "paredit" ]
+lib: {
+  configText,
+  alwaysEnsure ? false,
+  isOrgModeFile ? false,
+  alwaysTangle ? false,
+}: let
+  inherit
+    (lib.babel.emacs.fromElisp)
+    fromOrgModeBabelElisp'
+    fromElisp
+    ;
+  readFunction =
+    if isOrgModeFile
+    then
+      fromOrgModeBabelElisp' {
+        ":tangle" =
+          if alwaysTangle
+          then "yes"
+          else "no";
+      }
+    else fromElisp;
+
+  find = item: list:
+    if list == []
+    then []
+    else if builtins.head list == item
+    then list
+    else find item (builtins.tail list);
+
+  getKeywordValue = keyword: list: let
+    keywordList = find keyword list;
+  in
+    if keywordList != []
+    then let
+      keywordValue = builtins.tail keywordList;
+    in
+      if keywordValue != []
+      then builtins.head keywordValue
+      else true
+    else null;
+
+  isDisabled = item: let
+    disabledValue = getKeywordValue ":disabled" item;
+  in
+    if disabledValue == []
+    then false
+    else if builtins.isBool disabledValue
+    then disabledValue
+    else if builtins.isString disabledValue
+    then true
+    else false;
+
+  getName = item: let
+    ensureValue = getKeywordValue ":ensure" item;
+    usePackageName = builtins.head (builtins.tail item);
+  in
+    if builtins.isString ensureValue
+    then
+      if lib.hasPrefix ":" ensureValue
+      then usePackageName
+      else ensureValue
+    else if ensureValue == true || (ensureValue == null && alwaysEnsure)
+    then usePackageName
+    else [];
+
+  recurse = item:
+    if builtins.isList item && item != []
+    then let
+      packageManager = builtins.head item;
+    in
+      if builtins.elem packageManager ["use-package" "leaf"]
+      then
+        if !(isDisabled item)
+        then [packageManager (getName item)] ++ map recurse item
+        else []
+      else map recurse item
+    else [];
+in
+  lib.flatten (map recurse (readFunction configText))
