e9536191f9af1089beffe846dfcc6a8ce83c096a
Dysthesis <antheoraviel@protonmail.com>
2025-04-25 11:02:39 +1000

    Fix

--- lib/emacs/default.nix
+++ lib/emacs/default.nix
@@ -3,4 +3,5 @@ lib: {
   fromElisp = import (import ./npins).fromElisp {};
   parseUsePackages = import ./parseUsePackages.nix {inherit lib;};
   buildPackages = import ./buildPackages lib;
+  orgTangle = import ./orgTangle.nix;
 }
