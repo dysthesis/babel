e1c08c0bd2ef9478f7a802ea8854094c90fc2963
Dysthesis <antheoraviel@protonmail.com>
2025-04-28 17:17:05 +1000

    Get getExe to play nicely with mkWrapper

--- lib/pkgs/mkWrapper.nix
+++ lib/pkgs/mkWrapper.nix
@@ -9,18 +9,32 @@
 #                                  --run "${run}"
 # ''
 let
-  inherit (builtins)
-    isList
-    elemAt
-    ;
+  inherit (builtins) isList elemAt;
 in
-pkgs: package: postBuild:
-let
-  name = if isList package then elemAt package 0 else package;
-  paths = if isList package then package else [ package ];
-in
-pkgs.symlinkJoin {
-  inherit paths postBuild;
-  name = "${name}-wrapped";
-  buildInputs = [ pkgs.makeWrapper ];
-}
+  pkgs: package: postBuild:
+  # 1) Pick a “primary” package if the input is a list, otherwise use it directly.
+  let
+    target =
+      if isList package
+      then elemAt package 0
+      else package;
+    paths =
+      if isList package
+      then package
+      else [package];
+  in
+    # 2) Create the wrapper, copying over mainProgram from the original meta
+    pkgs.symlinkJoin {
+      inherit paths postBuild;
+
+      # name‐interpolation on a derivation uses its ‘name’ attribute
+      name = "${target}-wrapped";
+
+      buildInputs = [pkgs.makeWrapper];
+
+      # --- NEW: inherit the original mainProgram ---
+      # If target.meta.mainProgram is set, carry it through.
+      meta = {
+        inherit (target.meta) mainProgram;
+      };
+    }
